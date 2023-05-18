96a03e2d56f86eaa1a3420c2844e61d857368c63
Guilherme Henrique <gh.dourado77@gmail.com>
2023-05-18 15:39:33 -0300

    colocando fonte

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 import { User } from "lucide-react";
 import nlwLogo from '../assets/logo-spacetime.svg'
 import Image from "next/image";
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree,
+} from 'next/font/google'
+
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+
+const baiJamjuree = BaiJamjuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-jamjuree',
+})
 
 export default function Home() {
   return (
-    <main className="grid grid-cols-2 min-h-screen">
+    <main className={`${roboto.variable} ${baiJamjuree.variable} font-sans grid grid-cols-2 min-h-screen`}>
       <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
 
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 opacity-50 -translate-y-1/2 translate-x-1/2 rounded-full blur-full"></div>
@@ -21,10 +33,10 @@ export default function Home() {
         <div className=" space-y-5">
           <Image src={nlwLogo} alt="nlwLogo" />
           <div className="max-w-[420px] space-y-1">
-            <h1 className="mt-5 text-4xl font-bold text-gray-50 leading-tight">Sua cápsula do tempo</h1>
+            <h1 className="mt-5 text-4xl font-bold text-gray-50 leading-tight font-alt">Sua cápsula do tempo</h1>
             <p className="text-lg leading-relaxed">Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!</p>
           </div>
-          <a className="inline-block rounded-full bg-green-500 hover:bg-green-600 py-3 px-5 text-sm leading-none text-black font-medium" href="#">CADASTRAR LEMBRANÇA</a>
+          <a className="inline-block rounded-full bg-green-500 hover:bg-green-600 py-3 px-5 text-sm leading-none text-black font-alt font-medium" href="#">CADASTRAR LEMBRANÇA</a>
         </div>
 
         <div className="text-sm leading-relaxed text-gray-200">
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: 'var(--font-roboto)',
+        alt: 'var(--font-bai-jamjuree)',
+      },
       colors:{
         gray: {
           50:'#eaeaea',
