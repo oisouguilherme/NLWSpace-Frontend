5470f249b396305469e30b2cb8f6ff8b597dff66
Guilherme Henrique <gh.dourado77@gmail.com>
2023-05-21 18:15:00 -0300

    Terminando projeto Aula 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,10 @@ import { cookies } from 'next/headers'
 import Image from 'next/image'
 import Link from 'next/link'
 import { ArrowRight } from 'lucide-react'
+import dayjs from 'dayjs'
+import ptBr from 'dayjs/locale/pt-br'
 
+dayjs.locale(ptBr)
 
 interface Memory {
   id: string
@@ -40,7 +43,7 @@ export default async function Home() {
         return (
           <div key={memory.id} className="space-y-4">
             <time className="-ml-8 flex items-center gap-2 text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50">
-              
+            {dayjs(memory.createdAt).format('D[ de ] MMMM[, ] YYYY')}
             </time>
             <Image
               src={memory.coverUrl}
